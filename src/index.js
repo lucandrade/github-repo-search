b936ed103051c6318a441ab73cef93cc3695eaeb
Lucas Andrade <lucasandradedeveloper@gmail.com>
2020-06-16 14:36:23 +0100

    adding style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+import './Resources/Style/app.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <div className="app">Content</div>
