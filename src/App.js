6cb1f65652e79b3bc19cb419ecd66c20cc19409b
Lucas Andrade <lucasandradedeveloper@gmail.com>
2020-06-18 16:13:24 +0100

    adding app component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Form from "./Components/Form/Form";
+
+export default function App() {
+  return (
+    <>
+      <div className="app">
+        <Form />
+      </div>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+import App from "./App";
+
 import './Resources/Style/app.scss';
-import Form from "./Components/Form/Form";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="app">
-      <Form />
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
