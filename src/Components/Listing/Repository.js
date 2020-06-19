a1a3f2e219be7b44042aa35fa000f83dc231930e
Lucas Andrade <lucasandradedeveloper@gmail.com>
2020-06-19 16:55:37 +0100

    showing star and forks

--- src/Components/Listing/Repository.js
+++ src/Components/Listing/Repository.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Repository({ avatarUrl, url, name, description }) {
+export default function Repository({ stars, forks, avatarUrl, url, name, description }) {
   return (
     <div className="item">
       <div className="image">
@@ -8,6 +8,8 @@ export default function Repository({ avatarUrl, url, name, description }) {
       </div>
       <div className="details">
         <a href={url} target="_blank" className="title">{name}</a>
+        <span>Stars: {stars}</span>
+        <span>Forks: {forks}</span>
         <p>{description}</p>
       </div>
     </div>
--- src/Functions/Fetch.js
+++ src/Functions/Fetch.js
@@ -21,6 +21,8 @@ const fetch = async ({ query, sort, order }) => {
         description: i.description,
         url: i.html_url,
         avatarUrl: i.owner && i.owner.avatar_url ? i.owner.avatar_url : null,
+        stars: i.stargazers_count,
+        forks: i.forks,
       }));
     }
 
