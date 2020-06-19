65c1f5f012b0a45658d90b57fc951c6b998048aa
Lucas Andrade <lucasandradedeveloper@gmail.com>
2020-06-19 16:27:28 +0100

    using fetch function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
+import Fetch from "./Functions/Fetch";
 import Form from "./Components/Form/Form";
 import Repository from "./Components/Listing/Repository";
 
 export default function App() {
+  const [repos, setRepos] = useState([]);
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState({});
+  const fakeRef = useRef(true);
+
+  useEffect(() => {
+    if (fakeRef.current) {
+      fakeRef.current = false;
+    } else {
+      const runFetch = async () => {
+        setFetching(true);
+        const result = await Fetch(options);
+        setRepos(result);
+        setFetching(false);
+      };
+
+      if (options.query) {
+        runFetch();
+      }
+    }
+  }, [options, fakeRef]);
+
   return (
     <>
       <div className="app">
-        <Form onSearch={options => console.log('op', options)} />
-        <div className="loading">Loading</div>
+        <Form onSearch={setOptions} />
+        {fetching && <div className="loading">Loading</div>}
         <div className="list">
-          <Repository
-            avatarUrl={"https://avatars2.githubusercontent.com/u/96857?v=4"}
-            url={"https://avatars2.githubusercontent.com/u/96857?v=4"}
-            name={"First Repo"}
-            description={"here's a great description."} />
+          {repos.map(i =>  <Repository key={i.url} {...i} />)}
         </div>
       </div>
     </>
