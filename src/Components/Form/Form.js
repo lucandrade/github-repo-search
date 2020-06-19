fb38f037e5aec33098361dc288d2c0ee3b8247d1
Lucas Andrade <lucasandradedeveloper@gmail.com>
2020-06-19 20:23:18 +0100

    setting focus on input if empty

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -15,6 +15,8 @@ export default function Form({ disabled, onSearch }) {
     // Do not run when it's mounted
     if (fakeRef.current) {
       fakeRef.current = false;
+    } else if (!query) {
+      searchRef.current.focus();
     } else {
       onSearch({ query, sort, order });
     }
